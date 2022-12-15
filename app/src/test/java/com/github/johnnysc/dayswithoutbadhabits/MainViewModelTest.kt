21cb5071a28b876fac0d63be4f38e587ce902f98
hovhannes <johnnysc091@gmail.com>
2022-12-15 12:01:16 +0900

    unit test for viewModel

---
+++ app/src/test/java/com/github/johnnysc/dayswithoutbadhabits/MainViewModelTest.kt
@@ -0,0 +1,60 @@
+package com.github.johnnysc.dayswithoutbadhabits
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author Asatryan on 15.12.2022
+ */
+class MainViewModelTest {
+
+    @Test
+    fun test_0_days_and_reinit() {
+        val repository = FakeRepository(0)
+        val communication = FakeMainCommunication.Base()
+        val viewModel = MainViewModel(repository, communication)
+        viewModel.init(isFirstRun = true)
+        assertEquals(true, communication.checkCalledCount(1))
+        assertEquals(true, communication.isSame(UiState.ZeroDays))
+        viewModel.init(isFirstRun = false)
+        assertEquals(true, communication.checkCalledCount(1))
+    }
+
+    @Test
+    fun test_N_days_and_reinit() {
+        val repository = FakeRepository(5)
+        val communication = FakeMainCommunication.Base()
+        val viewModel = MainViewModel(repository, communication)
+        viewModel.init(isFirstRun = true)
+        assertEquals(true, communication.checkCalledCount(1))
+        assertEquals(true, communication.isSame(UiState.NDays(days = 5)))
+        viewModel.init(isFirstRun = false)
+        assertEquals(true, communication.checkCalledCount(1))
+    }
+}
+
+private class FakeRepository(private val days: Int) : MainRepository {
+
+    override fun days(): Int = days
+
+}
+
+interface FakeMainCommunication : MainCommunication.Put {
+
+    fun checkCalledCount(count: Int): Boolean
+    fun isSame(uiState: UiState): Boolean
+
+    class Base : FakeMainCommunication {
+        private lateinit var state: UiState
+        private var callCount = 0
+
+        override fun isSame(uiState: UiState): Boolean = state.equals(uiState)
+
+        override fun checkCalledCount(count: Int): Boolean = count == callCount
+
+        override fun put(value: UiState) {
+            callCount++
+            state = value
+        }
+    }
+}
