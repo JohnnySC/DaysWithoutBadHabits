a142d8042672c36d8bf21ad4655f716dabdad43e
hovhannes <johnnysc091@gmail.com>
2022-12-18 11:01:08 +0900

    interactor after unit tests

--- app/src/main/java/com/github/johnnysc/dayswithoutbadhabits/domain/NewMainInteractor.kt
+++ app/src/main/java/com/github/johnnysc/dayswithoutbadhabits/domain/NewMainInteractor.kt
@@ -3,19 +3,59 @@ package com.github.johnnysc.dayswithoutbadhabits.domain
 /**
  * @author Asatryan on 17.12.2022
  */
-interface NewMainInteractor : ResetCard {
-
-    fun cards(): List<Card>
+interface NewMainInteractor : CRUDCards {
 
     fun canAddNewCard(): Boolean
 
-    fun newCard(text: String): Card
+    class Base(
+        private val repository: Repository,
+        private val maxItemsCount: Int
+    ) : NewMainInteractor {
 
-    fun deleteCard(id: Long)
+        override fun cards(): List<Card> {
+            val cards = repository.cards()
+            return if (cards.size < maxItemsCount)
+                mutableListOf<Card>().apply {
+                    addAll(cards)
+                    add(Card.Add)
+                }
+            else
+                cards
+        }
 
-    fun updateCard(id: Long, newText: String)
+        override fun canAddNewCard(): Boolean {
+            val cards = repository.cards()
+            return cards.size < maxItemsCount
+        }
+
+        override fun newCard(text: String): Card {
+            return repository.newCard(text)
+        }
+
+        override fun deleteCard(id: Long) {
+            repository.deleteCard(id)
+        }
+
+        override fun updateCard(id: Long, newText: String) {
+            repository.updateCard(id, newText)
+        }
+
+        override fun resetCard(id: Long) {
+            repository.resetCard(id)
+        }
+    }
 }
 
 interface ResetCard {
     fun resetCard(id: Long)
+}
+
+interface CRUDCards : ResetCard {
+    fun cards(): List<Card>
+
+    fun newCard(text: String): Card
+
+    fun updateCard(id: Long, newText: String)
+
+    fun deleteCard(id: Long)
 }
---
+++ app/src/main/java/com/github/johnnysc/dayswithoutbadhabits/domain/Repository.kt
@@ -0,0 +1,7 @@
+package com.github.johnnysc.dayswithoutbadhabits.domain
+
+/**
+ * @author Asatryan on 18.12.2022
+ */
+interface Repository : CRUDCards {
+}
